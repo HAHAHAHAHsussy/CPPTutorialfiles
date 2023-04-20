be161a70c9e5ba140b3723d57c9f50424e90fc15
HAHAHAHAHsussy <101671592+HAHAHAHAHsussy@users.noreply.github.com>
2023-04-20 09:13:11 -0700

    Create logicoperators.cpp

---
+++ logicoperators.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int age = 64;
+    int money = 600; 
+    if(age>21 or money>500){
+        cout << "you are3 allowed in!" << endl;
+    }
+    
+    
+    return 0;
+}
+
