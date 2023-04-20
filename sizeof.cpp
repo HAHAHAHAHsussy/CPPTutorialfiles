775c416c72870380f49021cbb5e1c8cebd378b07
HAHAHAHAHsussy <101671592+HAHAHAHAHsussy@users.noreply.github.com>
2023-04-20 09:20:42 -0700

    Create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+//char variables take up 1 bit
+//int takes up 4 bites
+//double is 8 bites 
+//strings = 32 bites
+//array = datatype * howmany
+int main()
+{
+    double bucky[10];
+    cout << sizeof(bucky) << endl;
+}
